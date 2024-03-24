3346733b958e1a6fe683f5122840568809dff6a7
tracer-ysyx <tracer@ysyx.org>
2024-03-24 16:45:28 +0800

    > compile NEMU ysyx_22040000 张亦弛 Linux wapc 6.6.22-1-lts #1 SMP PREEMPT_DYNAMIC Sat, 16 Mar 2024 06:20:33 +0000 x86_64 GNU/Linux  16:45:28 up  1:36,  1 user,  load average: 0.72, 0.81, 0.86

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -18,6 +18,10 @@
 #include <cpu/difftest.h>
 #include <locale.h>
 
+#ifdef CONFIG_WATCHPOINT
+  #include "../monitor/sdb/watchpoint.h"
+#endif
+
 /* The assembly code of instructions executed is only output to the screen
  * when the number of instructions executed is less than this value.
  * This is useful when you use the `si' command.
@@ -38,6 +42,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+#ifdef CONFIG_WATCHPOINT
+
+#endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
