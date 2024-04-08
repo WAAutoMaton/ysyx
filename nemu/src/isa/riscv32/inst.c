387e0cf27bd9a5b0ea9e6b954e402e0a44a7ffc7
tracer-ysyx <tracer@ysyx.org>
2024-04-09 00:37:18 +0800

    > compile NEMU ysyx_22040000 张亦弛 Linux wapc 6.6.24-1-lts #1 SMP PREEMPT_DYNAMIC Wed, 03 Apr 2024 14:50:23 +0000 x86_64 GNU/Linux  00:37:18 up  4:56,  1 user,  load average: 0.79, 0.72, 0.73

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -43,7 +43,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
-    case TYPE_J:                   immJ(); break;
+    case TYPE_J:                   immJ(); printf("%u\n",*imm); break;
   }
 }
 
